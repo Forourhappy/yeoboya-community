e50795c543cf7de9727e4788481b8f71e9c6ec0e
Foh <foroh1504@gmail.com>
2025-04-04 18:30:53 +0900

    feat: 회원가입 임시 커밋밋

--- src/pages/auth/signup/SingupPage.tsx
+++ src/pages/auth/signup/SingupPage.tsx
@@ -1,23 +1,51 @@
-import { FormField, useFormField } from '~/shared/ui/shadcn/form.tsx';
-import { Form } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/shared/ui/shadcn/form.tsx";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "~/shared/ui/shadcn/input";
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .max(10, {
+      message: "이름은 10자 이하로 입력해주세요.",
+    })
+    .nonempty("이름을 입력해주세요."),
+});
 
 export const SignupPage = () => {
-	const nameField = useFormField();
-	return (
-		<div>
-			회원가입
-			<Form>
-				<FormField
-					{...nameField}
-					render={({ input, meta }) => (
-						<div>
-							<label htmlFor={input.id}>이름</label>
-							<input {...input} />
-							{meta.error && <div>{meta.error}</div>}
-						</div>
-					)}
-				/>
-			</Form>
-		</div>
-	);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+  console.log(form);
+  return (
+    <div>
+      <h2>회원가입</h2>
+      <Form {...form}>
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>이름</FormLabel>
+              <FormControl>
+                <Input placeholder="asdf" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </Form>
+    </div>
+  );
 };
